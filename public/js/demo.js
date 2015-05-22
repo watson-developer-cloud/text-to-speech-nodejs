74cb4c3e533a423637ec44f2d9400a2ac64cad09
ERIC S. BULLINGTON <esbullin@us.ibm.com>
2015-05-22 12:45:52 -0400

    Add partial implementation of SpeechSynthesis API to demo to get voices

--- public/js/demo.js
+++ public/js/demo.js
@@ -24,17 +24,31 @@ $(document).ready(function() {
   var textChanged = false,
     spanishText = 'El servicio de Voz a Texto utiliza la tecnología de síntesis de voz de IBM para convertir texto en Inglés o Español en una señal de audio. El audio es enviado de vuelta al cliente con un retraso mínimo. El servicio puede ser accedido a través de una interfaz REST.',
     englishText = 'The Text to Speech service uses IBM\'s speech synthesis capabilities to convert English or Spanish text to an audio signal. The audio is streamed back to the client with minimal delay. The service can be accessed via a REST interface.',
-		frenchText = "Le service TTS d'IBM profite de ses capacités de synthèse de la parole à partir du texte pour transformer le texte à un signal audio. Le signal audio est ensuite passé au client dans un délai minimal. On peut appeler Le service via une interface REST",
-		germanText = "German text placeholder",
-		italianText = "Italian text placeholder";
+		frenchText = 'Le service TTS d\'IBM profite de ses capacités de synthèse de la parole à partir du texte pour transformer le texte à un signal audio. Le signal audio est ensuite passé au client dans un délai minimal. On peut appeler Le service via une interface REST',
+		germanText = 'German text placeholder',
+		italianText = 'Italian text placeholder';
+
 
   $('#textArea').val(englishText);
 
+  $('#textArea').change(function(){
+    textChanged = true;
+  });
+
   $('#voice').change(function(){
     if (!textChanged) {
-      switch($(this).val()) {
-				case 'es-ES_EnriqueVoice':
+      switch($(this).val().substring(0, 2)) {
+				case 'es':
 					$('#textArea').val(spanishText);
+					break;
+				case 'fr':
+					$('#textArea').val(frenchText);
+					break;
+				case 'de':
+					$('#textArea').val(germanText);
+					break;
+				case 'it':
+					$('#textArea').val(italianText);
 					break;
 				default:
 					$('#textArea').val(englishText);
@@ -43,9 +57,20 @@ $(document).ready(function() {
     }
   });
 
-  $('#textArea').change(function(){
-    textChanged = true;
-  });
+	var tts = new SpeechSynthesis();
+	tts.onvoiceschanged = function() {
+		var voices = tts.getVoices();
+		console.log('voices', voices);
+		$.each(voices, function(idx, voice) {
+			var voiceName = voice.name.substring(6, voice.name.length - 5);
+			var optionText = voice._gender + ' voice: ' + voiceName + ' (' + voice.lang + ')';
+			$('#voice')
+			.append($('<option>', { value : voice.name })
+				.prop('selected', voice.name === 'en-US_MichaelVoice' ? true : false)
+				.text(optionText));
+		});
+	};
+
 
   // IE and Safari not supported disabled Speak button
   if ($('body').hasClass('ie') || $('body').hasClass('safari')) {
@@ -84,13 +109,13 @@ $(document).ready(function() {
       xhr.open('POST', url, true);
       xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
       xhr.setRequestHeader("Accept", "audio/ogg; codecs=opus");
-      // xhr.responseType = 'blob';
-      // xhr.onreadystatechange = function() {
-      //   if (xhr.readyState === 4) {
-      //     var blob = new Blob([this.response], {type: 'audio/ogg'});
-      //     saveAs(blob, "transcript.ogg");
-      //   }
-      // };
+      xhr.responseType = 'blob';
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+          var blob = new Blob([this.response], {type: 'audio/ogg'});
+          saveAs(blob, "transcript.ogg");
+        }
+      };
       xhr.send(jsonData);
     }
   });
