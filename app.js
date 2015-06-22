df2464f48e2c1e1a55dc2545870e4ea8819cabc2
ERIC S. BULLINGTON <esbullin@us.ibm.com>
2015-06-22 16:49:32 -0400

    Checkpoint commit

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ var express = require('express'),
   bodyParser   = require('body-parser'),
   bluemix = require('./config/bluemix'),
   watson = require('watson-developer-cloud'),
+  url = require('url'),
   path = require('path'),
   // environmental variable points to demo's json config file
   config = JSON.parse(process.env.WATSON_CONFIG),
@@ -50,23 +51,45 @@ app.get('/', function(req, res) {
 //     res.send(body);
 //   })
 // });
+
+
+// Get token from Watson using your credentials
+app.get('/download', function(req, res) {
+  res.headers['content-disposition'] = 'attachment; filename=transcript.ogg';
+  var queryString = url.parse(req.url).query;
+  request.get({'url': 
+    'https://' 
+    + credentials.hostname 
+    + '/text-to-speech-beta/api/v1/synthesize?'
+    + queryString,
+    'auth': {
+      'user': credentials.username,
+    'pass': credentials.password,
+    'sendImmediately': true
+    }
+  }, function(err, response, body) {
+    res.send(body);
+  }
+  );
+});
+
 // Get token from Watson using your credentials
 app.get('/token', function(req, res) {
-    console.log('fetching token');
-    request.get({'url': 
-      'https://' 
-        + credentials.hostname 
-        + '/authorization/api/v1/token?url=' 
-        + 'https://' + credentials.hostname + '/text-to-speech-beta/api',
-      'auth': {
-        'user': credentials.username,
-        'pass': credentials.password,
-        'sendImmediately': true
-      }
-    }, function(err, response, body) {
-      res.send(body);
+  console.log('fetching token');
+  request.get({'url': 
+    'https://' 
+    + credentials.hostname 
+    + '/authorization/api/v1/token?url=' 
+    + 'https://' + credentials.hostname + '/text-to-speech-beta/api',
+    'auth': {
+      'user': credentials.username,
+    'pass': credentials.password,
+    'sendImmediately': true
     }
-    );
+  }, function(err, response, body) {
+    res.send(body);
+  }
+  );
 });
 
 // Add error handling in dev
@@ -78,45 +101,3 @@ var port = process.env.VCAP_APP_PORT || 3000;
 app.listen(port);
 
 console.log('listening at:', port);
-
-var http = require('http'),
-    httpProxy = require('http-proxy');
-
-// Create a proxy server with custom application logic
-//
-var proxy = httpProxy.createProxyServer({});
-
-//
-// Create your custom server and just call `proxy.web()` to proxy
-// a web request to the target passed in the options
-// also you can use `proxy.ws()` to proxy a websockets request
-//
-var server = http.createServer(function(req, res) {
-  // You can define here your custom logic to handle the request
-  // and then proxy the request.
-  req.headers['Authorization'] = 'Basic ' + new Buffer(process.env.BASIC_AUTH_STAGING).toString('base64');
-  proxy.web(req, res, { target: 'https://stream-s.watsonplatform.net', secure: false });
-
-  res.oldWriteHead = res.writeHead;
-  res.writeHead = function(statusCode, headers) {
-    /* add logic to change headers here */
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Method', '*');
-
-    // old way: might not work now
-    // as headers param is not always provided
-    // https://github.com/nodejitsu/node-http-proxy/pull/260/files
-    // headers['foo'] = 'bar';       
-
-    res.oldWriteHead(statusCode, headers);
-  }
-  // and then proxy the request.
-  var auth = 'Basic ' + new Buffer(process.env.BASIC_AUTH_STAGING).toString('base64');
-  console.log('auth header', auth);
-  req.headers['Authorization'] = 'Basic ' + new Buffer(process.env.BASIC_AUTH_STAGING).toString('base64');
-  proxy.web(req, res, { target: 'https://stream-s.watsonplatform.net', secure: false });
-
-});
-
-console.log("listening on port 8000")
-server.listen(8000);
