4e0cda79a058f7efd9020f4d17b47e5e5ed50d8e
ERIC S. BULLINGTON <esbullin@us.ibm.com>
2015-06-24 14:46:04 -0400

    Bluemix module formatting

--- config/bluemix.js
+++ config/bluemix.js
@@ -25,18 +25,18 @@
  * name is not found in VCAP_SERVICES
  */
 module.exports.getServiceCreds = function(name) {
-    if (process.env.VCAP_SERVICES) {
-        var services = JSON.parse(process.env.VCAP_SERVICES);
-        for (var service_name in services) {
-            if (service_name.indexOf(name) === 0) {
-                var service = services[service_name][0];
-                return {
-                    url: service.credentials.url,
-                    username: service.credentials.username,
-                    password: service.credentials.password
-                };
-            }
-        }
+  if (process.env.VCAP_SERVICES) {
+    var services = JSON.parse(process.env.VCAP_SERVICES);
+    for (var service_name in services) {
+      if (service_name.indexOf(name) === 0) {
+        var service = services[service_name][0];
+        return {
+          url: service.credentials.url,
+            username: service.credentials.username,
+            password: service.credentials.password
+        };
+      }
     }
-    return {};
+  }
+  return {};
 };
