b294f0aa588f9eb6edbf9a88fc234a3e95483c23
Eric S. Bullington <eric.s.bullington@gmail.com>
2015-07-10 10:53:51 -0400

    Change voice list format: resolves #8

--- public/js/constants.js
+++ public/js/constants.js
@@ -26,11 +26,11 @@ window.SPEECH_SYNTHESIS_VOICES = {
         "description": "English language with US dialect, female.  Higher-quality uncompressed Allison voice."
     }, 
     {
-      "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/es-ES_LauraVoice", 
+      "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/en-US_LisaVoice", 
       "gender": "female", 
-      "name": "es-ES_LauraVoice", 
-      "language": "es-ES", 
-      "description": "LLSS Spanish female Language with Castilian dialect. Laura talent"
+      "name": "en-US_LisaVoice", 
+      "language": "en-US", 
+      "description": "English language with US dialect, female.  Higher-quality uncompressed Lisa voice."
     }, 
     {
       "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/en-US_MichaelVoice", 
@@ -40,32 +40,11 @@ window.SPEECH_SYNTHESIS_VOICES = {
       "description": "English language with US dialect, male voice.  The voice used by Watson Jeopardy system."
     }, 
     {
-      "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/es-US_SofiaVoice", 
+      "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/fr-FR_ReneeVoice", 
       "gender": "female", 
-      "name": "es-US_SofiaVoice", 
-      "language": "es-US", 
-      "description": "Spanish language with US dialect, female.  Higher-quality uncompressed Sofia voice."
-    }, 
-    {
-      "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/en-GB_KateVoice", 
-      "gender": "female", 
-      "name": "en-GB_KateVoice", 
-      "language": "en-GB", 
-      "description": "English language with UK dialect, female.  Higher-quality uncompressed Kate voice."
-    }, 
-    {
-      "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/en-US_LisaVoice", 
-      "gender": "female", 
-      "name": "en-US_LisaVoice", 
-      "language": "en-US", 
-      "description": "English language with US dialect, female.  Higher-quality uncompressed Lisa voice."
-    }, 
-    {
-      "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/es-ES_EnriqueVoice", 
-      "gender": "male", 
-      "name": "es-ES_EnriqueVoice", 
-      "language": "es-ES", 
-      "description": "LLSS Spanish Male Language with Castilian dialect. Enrique talent"
+      "name": "fr-FR_ReneeVoice", 
+      "language": "fr-FR", 
+      "description": "French language with French dialect, female.  Higher-quality uncompressed Renee voice."
     }, 
     {
       "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/de-DE_BirgitVoice", 
@@ -82,19 +61,32 @@ window.SPEECH_SYNTHESIS_VOICES = {
       "description": "German language with German dialect, male.  Higher-quality uncompressed Dieter voice."
     }, 
     {
-      "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/fr-FR_ReneeVoice", 
-      "gender": "female", 
-      "name": "fr-FR_ReneeVoice", 
-      "language": "fr-FR", 
-      "description": "French language with French dialect, female.  Higher-quality uncompressed Renee voice."
-    }, 
-    {
       "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/it-IT_FrancescaVoice", 
       "gender": "female", 
       "name": "it-IT_FrancescaVoice", 
       "language": "it-IT", 
       "description": "Italian language with Italian dialect, female.  Higher-quality uncompressed Francesca voice."
+    },
+    {
+      "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/es-ES_EnriqueVoice", 
+      "gender": "male", 
+      "name": "es-ES_EnriqueVoice", 
+      "language": "es-ES", 
+      "description": "LLSS Spanish Male Language with Castilian dialect. Enrique talent"
+    },
+    {
+      "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/es-ES_LauraVoice", 
+      "gender": "female", 
+      "name": "es-ES_LauraVoice", 
+      "language": "es-ES", 
+      "description": "LLSS Spanish female Language with Castilian dialect. Laura talent"
+    }, 
+    {
+      "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/es-US_SofiaVoice", 
+      "gender": "female", 
+      "name": "es-US_SofiaVoice", 
+      "language": "es-US", 
+      "description": "Spanish language with US dialect, female.  Higher-quality uncompressed Sofia voice."
     }
-
   ]
 }
