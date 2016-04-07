368067c3ec4f1ed5c4897af4765b9defb2c5a44d
German Attanasio Ruiz <germanattanasio@gmail.com>
2016-04-07 14:48:39 -0400

    Added michael voice

--- public/js/constants.js
+++ public/js/constants.js
@@ -9,7 +9,7 @@ var spanishText = "Consciente de su patrimonio espiritual y moral, la Unión est
     germanText = "In dem Bewusstsein ihres geistig-religiösen und sittlichen Erbes gründet sich die Union auf die unteilbaren und universellen Werte der Würde des Menschen, der Freiheit, der Gleichheit und der Solidarität. Sie beruht auf den Grundsätzen der Demokratie und der Rechtsstaatlichkeit. Sie stellt den Menschen in den Mittelpunkt ihres Handelns, indem sie die Unionsbürgerschaft und einen Raum der Freiheit, der Sicherheit und des Rechts begründet.",
     italianText = "Consapevole del suo patrimonio spirituale e morale, l'Unione si fonda sui valori indivisibili e universali della dignità umana, della libertà, dell'uguaglianza e della solidarietà; essa si basa sul principio della democrazia e sul principio dello Stato di diritto. Pone la persona al centro della sua azione istituendo la cittadinanza dell'Unione e creando uno spazio di libertà, sicurezza e giustizia.",
     japaneseText = "精神的、そして道徳的な遺産を意識的に受け継いで、人間の尊厳、自由、平等、連帯の不可分で普遍的な価値を大前提として連合は設立される。すなわち、連合は民主主義と法治の原則に立脚する。連合は市民権を確立し、自由と安全、正義が確保された地域を創造することによって、その組織活動の中心に個人を置く。",
-    brazilianPortugueseText = "Consciente do seu patrimônio espiritual e moral, a União é fundamentada nos valores indivisíveis e universais da dignidade humana, liberdade, igualdade e solidariedade; é baseada nos princípios da democracia e estado de direito. Ela coloca o indivíduo no centro de suas ações, ao instituir a cidadania da União e ao criar um espaço de liberdade, segurança e justiça.";    
+    brazilianPortugueseText = "Consciente do seu patrimônio espiritual e moral, a União é fundamentada nos valores indivisíveis e universais da dignidade humana, liberdade, igualdade e solidariedade; é baseada nos princípios da democracia e estado de direito. Ela coloca o indivíduo no centro de suas ações, ao instituir a cidadania da União e ao criar um espaço de liberdade, segurança e justiça.";
 
 // Sample text values with SSML
 var spanishSSML = "Spanish SSML is not presently supported.",
@@ -25,100 +25,108 @@ var spanishSSML = "Spanish SSML is not presently supported.",
 window.SPEECH_SYNTHESIS_VOICES = {
     voices: [
     {
-        "name": "en-US_AllisonVoice", 
-        "language": "en-US", 
-        "customizable": true, 
-        "gender": "female, expressive", 
-        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/en-US_AllisonVoice", 
+        "name": "en-US_AllisonVoice",
+        "language": "en-US",
+        "customizable": true,
+        "gender": "female, expressive",
+        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/en-US_AllisonVoice",
         "description": "Allison: American English female voice."
     },
     {
-        "name": "en-US_LisaVoice", 
-        "language": "en-US", 
-        "customizable": true, 
-        "gender": "female", 
-        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/en-US_LisaVoice", 
+        "name": "en-US_MichaelVoice",
+        "language": "en-US",
+        "customizable": true,
+        "gender": "male",
+        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/en-US_MichaelVoice",
+        "description": "Michael: American English male voice."
+    },
+    {
+        "name": "en-US_LisaVoice",
+        "language": "en-US",
+        "customizable": true,
+        "gender": "female",
+        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/en-US_LisaVoice",
         "description": "Lisa: American English female voice."
     },
     {
-        "name": "en-GB_KateVoice", 
-        "language": "en-GB", 
-        "customizable": false, 
-        "gender": "female", 
-        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/en-GB_KateVoice", 
+        "name": "en-GB_KateVoice",
+        "language": "en-GB",
+        "customizable": false,
+        "gender": "female",
+        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/en-GB_KateVoice",
         "description": "Kate: British English female voice."
     },
     {
-        "name": "fr-FR_ReneeVoice", 
-        "language": "fr-FR", 
-        "customizable": false, 
-        "gender": "female", 
-        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/fr-FR_ReneeVoice", 
+        "name": "fr-FR_ReneeVoice",
+        "language": "fr-FR",
+        "customizable": false,
+        "gender": "female",
+        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/fr-FR_ReneeVoice",
         "description": "Renee: French (français) female voice."
-    }, 
+    },
     {
-        "name": "de-DE_BirgitVoice", 
-        "language": "de-DE", 
-        "customizable": false, 
-        "gender": "female", 
-        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/de-DE_BirgitVoice", 
+        "name": "de-DE_BirgitVoice",
+        "language": "de-DE",
+        "customizable": false,
+        "gender": "female",
+        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/de-DE_BirgitVoice",
         "description": "Birgit: Standard German of Germany (Standarddeutsch) female voice."
     },
     {
-        "name": "de-DE_DieterVoice", 
-        "language": "de-DE", 
-        "customizable": false, 
-        "gender": "male", 
-        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/de-DE_DieterVoice", 
+        "name": "de-DE_DieterVoice",
+        "language": "de-DE",
+        "customizable": false,
+        "gender": "male",
+        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/de-DE_DieterVoice",
         "description": "Dieter: Standard German of Germany (Standarddeutsch) male voice."
     },
     {
-        "name": "it-IT_FrancescaVoice", 
-        "language": "it-IT", 
-        "customizable": false, 
-        "gender": "female", 
-        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/it-IT_FrancescaVoice", 
+        "name": "it-IT_FrancescaVoice",
+        "language": "it-IT",
+        "customizable": false,
+        "gender": "female",
+        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/it-IT_FrancescaVoice",
         "description": "Francesca: Italian (italiano) female voice."
     },
     {
-        "name": "ja-JP_EmiVoice", 
-        "language": "ja-JP", 
-        "customizable": false, 
-        "gender": "female", 
-        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/ja-JP_EmiVoice", 
+        "name": "ja-JP_EmiVoice",
+        "language": "ja-JP",
+        "customizable": false,
+        "gender": "female",
+        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/ja-JP_EmiVoice",
         "description": "Emi: Japanese (日本語) female voice."
-    }, 
+    },
     {
-        "name": "pt-BR_IsabelaVoice", 
-        "language": "pt-BR", 
-        "customizable": false, 
-        "gender": "female", 
-        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/pt-BR_IsabelaVoice", 
+        "name": "pt-BR_IsabelaVoice",
+        "language": "pt-BR",
+        "customizable": false,
+        "gender": "female",
+        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/pt-BR_IsabelaVoice",
         "description": "Isabela: Brazilian Portuguese (português brasileiro) female voice."
     },
     {
-        "name": "es-ES_EnriqueVoice", 
-        "language": "es-ES", 
-        "customizable": false, 
-        "gender": "male", 
-        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/es-ES_EnriqueVoice", 
+        "name": "es-ES_EnriqueVoice",
+        "language": "es-ES",
+        "customizable": false,
+        "gender": "male",
+        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/es-ES_EnriqueVoice",
         "description": "Enrique: Castilian Spanish (español castellano) male voice."
     },
     {
-        "name": "es-ES_LauraVoice", 
-        "language": "es-ES", 
-        "customizable": false, 
-        "gender": "female", 
-        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/es-ES_LauraVoice", 
+        "name": "es-ES_LauraVoice",
+        "language": "es-ES",
+        "customizable": false,
+        "gender": "female",
+        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/es-ES_LauraVoice",
         "description": "Laura: Castilian Spanish (español castellano) female voice."
     },
     {
-        "name": "es-US_SofiaVoice", 
-        "language": "es-US", 
-        "customizable": false, 
-        "gender": "female", 
-        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/es-US_SofiaVoice", 
+        "name": "es-US_SofiaVoice",
+        "language": "es-US",
+        "customizable": false,
+        "gender": "female",
+        "url": "https://stream.watsonplatform.net/text-to-speech/api/v1/voices/es-US_SofiaVoice",
         "description": "Sofia: North American Spanish (español norteamericano) female voice."
-    } 
+    }
   ]
 }
--- public/js/demo.js
+++ public/js/demo.js
@@ -38,12 +38,12 @@ $(document).ready(function() {
   }
 
   function onCanplaythrough() {
-    console.log("onCanplaythrough");
+    console.log('onCanplaythrough');
     var audio = $('.audio').get(0);
-    audio.removeEventListener("canplaythrough", onCanplaythrough); 
+    audio.removeEventListener('canplaythrough', onCanplaythrough);
     try {
       audio.currentTime = 0;
-    } 
+    }
     catch(ex) {
       // ignore. Firefox just freaks out here for no apparent reason.
     }
@@ -51,11 +51,11 @@ $(document).ready(function() {
     audio.muted = false;
     $('.result').show();
     $('.error-row').css('visibility','hidden');
-    $('html, body').animate({scrollTop: $(".audio").offset().top}, 500);
+    $('html, body').animate({scrollTop: $('.audio').offset().top}, 500);
     $('body').css('cursor', 'default');
     $('.speak-button').css('cursor', 'pointer');
   }
-  
+
   function synthesizeRequest(options, audio) {
     var sessionPermissions = JSON.parse(localStorage.getItem('sessionPermissions')) ? 0 : 1;
     var downloadURL = '/api/synthesize' +
@@ -71,7 +71,7 @@ $(document).ready(function() {
     audio.pause();
     audio.src = downloadURL;
     enableButtons(true);
-    audio.addEventListener("canplaythrough", onCanplaythrough);
+    audio.addEventListener('canplaythrough', onCanplaythrough);
     audio.muted = true;
     audio.play();
     $('body').css('cursor', 'wait');
@@ -94,17 +94,17 @@ $(document).ready(function() {
     }
     return true;
   }
-  
+
   function disableButtons() {
     $('.download-button').prop('disabled', true);
     $('.speak-button').prop('disabled', true);
   }
-  
+
   function enableButtons() {
     $('.download-button').prop('disabled', false);
     $('.speak-button').prop('disabled', false);
   }
-  
+
   function showVoices(voices) {
 
     var currentTab = 'Text';
@@ -117,9 +117,9 @@ $(document).ready(function() {
 
     $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
       currentTab = $(e.target).text();
-      audio.src = "";
+      audio.src = '';
       audio.controls = false;
-      if(currentTab == "SSML" && isSSMLSupported() == false) {
+      if(currentTab === 'SSML' && isSSMLSupported() === false) {
         disableButtons();
       }
       else {
@@ -174,16 +174,16 @@ $(document).ready(function() {
       $('#dropdownMenuDefault').empty().text(newVoiceDescription);
       $('#dropdownMenu1').dropdown('toggle');
       $('#ssml_caption').text('SSML');
-      audio.src = ""; // to stop currently playing audio
+      audio.src = ''; // to stop currently playing audio
       audio.controls = false; // to hide the player
       enableButtons();
- 
+
       var lang = voice.substring(0, 2);
         switch(lang) {
           case 'es':
             $('#textArea').val(spanishText);
             $('#ssmlArea').val(spanishSSML);
-            if(currentTab == "SSML" || currentTab == "Expressive SSML") disableButtons();
+            if(currentTab == 'SSML' || currentTab == 'Expressive SSML') disableButtons();
             break;
           case 'fr':
             $('#textArea').val(frenchText);
@@ -196,17 +196,17 @@ $(document).ready(function() {
           case 'it':
             $('#textArea').val(italianText);
             $('#ssmlArea').val(italianSSML);
-            if(currentTab == "SSML" || currentTab == "Expressive SSML") disableButtons();
+            if(currentTab === 'SSML' || currentTab === 'Expressive SSML') disableButtons();
             break;
           case 'ja':
             $('#textArea').val(japaneseText);
             $('#ssmlArea').val(japaneseSSML);
-            if(currentTab == "SSML" || currentTab == "Expressive SSML") disableButtons();
+            if(currentTab === 'SSML' || currentTab === 'Expressive SSML') disableButtons();
             break;
           case 'pt':
             $('#textArea').val(brazilianPortugueseText);
             $('#ssmlArea').val(brazilianPortugueseSSML);
-            if(currentTab == "SSML" || currentTab == "Expressive SSML") disableButtons();
+            if(currentTab === 'SSML' || currentTab === 'Expressive SSML') disableButtons();
             break;
           case 'en':
             if(voice === 'en-US_AllisonVoice') {
@@ -238,7 +238,7 @@ $(document).ready(function() {
     }
 
     $('.audio').on('error', function (err) {
-      if(this.src == this.baseURI) {
+      if(this.src === this.baseURI) {
         console.log('audio.src was reset');
         return;
       }
