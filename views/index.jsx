914b8e5bc9d609f1453b3af41aa3b055d19e899d
German Attanasio Ruiz <germanattanasio@gmail.com>
2017-03-02 14:05:49 -0500

    :bug: Added props

--- views/index.jsx
+++ views/index.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import Layout from './layout.jsx';
 import Demo from './demo.jsx';
 
-export default function () {
+export default function (props) {
   return (<Layout bluemixAnalytics={props.BLUEMIX_ANALYTICS} ><Demo/></Layout>); // eslint-disable-line
 }
