88555624e51905094d2f5ca568e897e2a6bc5266
LEONID RACHEVSKY <rachevsk@us.ibm.com>
2019-02-25 18:33:41 -0500

    wip

--- voices.js
+++ voices.js
@@ -11,10 +11,12 @@ const PT_TEXT = 'Consciente do seu patrimônio espiritual e moral, a União é f
 const ES_SSML = '<p><s>Consciente de su patrimonio espiritual y moral<break time="300ms"/>, la Unión está fundada sobre los valores indivisibles y universales de la dignidad humana, <prosody rate="-15%"> la libertad, la igualdad y la solidaridad, </prosody> y se basa en los principios de la democracia y el Estado de Derecho<break time="500ms"/>.</s> <s><prosody rate="+20%">Al instituir la ciudadanía de la Unión </prosody> y crear un espacio de libertad, seguridad y justicia, sitúa a la persona en el centro de su actuación.</s></p>';
 const FR_SSML = '<p><s>Consciente de son patrimoine spirituel et moral<break time="300ms"/>,  l\'Union se fonde sur les valeurs indivisibles et universelles de dignité humaine,  <prosody rate="-15%"> de  liberté, d\'égalité et de solidarité; </prosody> elle repose sur le principe de la démocratie et le principe de l\'État de droit <break time="500ms"/>. Elle place la personne au coeur de son action en instituant la citoyenneté de l\'Union et en créant un espace de liberté, de sécurité et de justice.</s></p>';
 const US_SSML = '<p><s>Conscious of its spiritual and moral heritage <break time="300ms"/>, the Union is founded on the indivisible, universal values of <prosody rate="-15%">human dignity, freedom, equality and solidarity.</prosody> It is based on the principles of democracy and the rule of law <break time="500ms"/>. </s> <s> It places the individual at the heart of its activities, <prosody rate="+15%">by establishing the citizenship of the Union</prosody> and by creating an area of freedom, security and justice.</s></p>';
+const US_SSML_DNN = '<p><s>Conscious of its spiritual and moral heritage <break time="300ms"/>, the Union is founded on the indivisible, universal values of human dignity, freedom, equality and solidarity. It is based on the principles of democracy and the rule of law <break time="500ms"/>. </s> <s> It places the individual at the heart of its activities, by establishing the citizenship of the Union and by creating an area of freedom, security and justice.</s></p>';
 const US_SSML_EXPRESSIVE = '<speak>I have been assigned to handle your order status request.<express-as type="Apology"> I am sorry to inform you that the items you requested are back-ordered. We apologize for the inconvenience.</express-as><express-as type="Uncertainty"> We don\'t know when those items will become available. Maybe next week but we are not sure at this time.</express-as><express-as type="GoodNews">Because we want you to be a happy customer, management has decided to give you a 50% discount! </express-as></speak>';
 const US_GB_SSML = '<p><s>Conscious of its spiritual and moral heritage <break time="300ms"/>, the Union is founded on the indivisible, universal values of <prosody rate="-15%">human dignity, freedom, equality and solidarity.</prosody> It is based on the principles of democracy and the rule of law <break time="500ms"/>. </s> <s> It places the individual at the heart of its activities, <prosody rate="+15%">by establishing the citizenship of the Union</prosody> and by creating an area of freedom, security and justice.</s></p>';
 const DE_SSML = '<p><s>In dem Bewusstsein ihres geistig-religiösen und sittlichen <phoneme alphabet="ibm" ph=".1R.0bIs">Erbes</phoneme> <break time="300ms"/> gründet sich die Union auf die <prosody rate="-15%">unteilbaren und universellen  Werte der Würde des Menschen, der Freiheit, der Gleichheit und der Solidarität.</prosody> Sie beruht auf den Grundsätzen der Demokratie und der Rechtsstaatlichkeit<break time="1s"/>. Sie stellt den Menschen in den Mittelpunkt ihres <phoneme alphabet="ibm" ph=".1hAn.0d@lns"> Handelns</phoneme>, <prosody rate="+10%">indem sie die Unionsbürgerschaft und einen Raum der Freiheit, der Sicherheit und des Rechts begründet.</prosody></s></p>';
 const IT_SSML = '<p><s>Consapevole del suo patrimonio spirituale e morale<break time="300ms"/>, l\'Unione si fonda sui valori indivisibili e universali della dignità umana, <prosody rate="-15%">della libertà, dell\'uguaglianza e della solidarietà; </prosody> essa si basa sul principio della democrazia e sul principio dello Stato di diritto<break time="500ms"/>.</s><s> Pone la persona al centro della sua azione istituendo la cittadinanza dell\'Unione e creando uno spazio di libertà, sicurezza e giustizia.</s></p>';
+const IT_SSML_DNN = '<p><s>Consapevole del suo patrimonio spirituale e morale<break time="300ms"/>, l\'Unione si fonda sui valori indivisibili e universali della dignità umana, della libertà, dell\'uguaglianza e della solidarietà; essa si basa sul principio della democrazia e sul principio dello Stato di diritto<break time="500ms"/>.</s><s> Pone la persona al centro della sua azione istituendo la cittadinanza dell\'Unione e creando uno spazio di libertà, sicurezza e giustizia.</s></p>';
 const JP_SSML = '<p><s>精神的、そして道徳的な遺産を<break time="300ms"/>意識的に受け継いで、<prosody rate="-15%">人間の尊厳、自由、平等、連帯の不可分で普遍的な価値を大前提として</prosody>連合は設立される。すなわち、連合は民主主義と法治の原則に立脚する。<break time="500ms"/>連合は市民権を確立し、自由と安全、正義が確保された地域を創造することによって、その組織活動の中心に個人を置く。</s></p>';
 const PT_SSML = '<p><s>Consciente do seu patrimônio espiritual e moral<break time="300ms"/>, a União é fundamentada nos valores indivisíveis e universais da dignidade humana, <prosody rate="-15%">liberdade, igualdade e solidariedade; </prosody> é baseada nos princípios da democracia e estado de direito<break time="500ms"/>. </s> <s> <prosody rate="+15%">Ela coloca o indivíduo no centro de suas ações, </prosody> ao instituir a cidadania da União e ao criar um espaço de liberdade, segurança e justiça.</s></p>';
 
@@ -33,7 +35,7 @@ const voices = [
     option: 'Brazilian Portuguese (pt-BR): Isabela (female)',
     customizable: true,
     gender: 'female',
-    url: 'https://stream.watsonplatform.net/text-to-speech/api/v1/voices/pt-BR_IsabelaVoice',
+    url: 'https://gateway-d.watsonplatform.net/text-to-speech/api/v1/voices/pt-BR_IsabelaVoice',
     supported_features: {
       voice_transformation: false,
       custom_pronunciation: true,
@@ -49,7 +51,7 @@ const voices = [
     option: 'Japanese (ja-JP): Emi (female)',
     customizable: true,
     gender: 'female',
-    url: 'https://stream.watsonplatform.net/text-to-speech/api/v1/voices/ja-JP_EmiVoice',
+    url: 'https://gateway-d.watsonplatform.net/text-to-speech/api/v1/voices/ja-JP_EmiVoice',
     supported_features: {
       voice_transformation: false,
       custom_pronunciation: true,
@@ -60,12 +62,28 @@ const voices = [
     },
     description: 'Emi: Japanese (日本語) female voice.',
   }, {
+    name: 'en-US_MichaelV2Voice',
+    language: 'en-US',
+    option: 'American English (en-US): Michael (male, expressive, dnn technology)',
+    customizable: true,
+    gender: 'male',
+    url: 'https://gateway-d.watsonplatform.net/text-to-speech/api/v1/voices/en-US_MichaelV2Voice',
+    supported_features: {
+       voice_transformation: false,
+       custom_pronunciation: true
+    },
+    demo: {
+      text: US_TEXT,
+      ssml: US_SSML_DNN
+    },
+    description: 'Michael: American English male voice. Dnn technology.',
+ }, {
     name: 'en-US_AllisonVoice',
     option: 'American English (en-US): Allison (female, expressive, transformable)',
     language: 'en-US',
     customizable: true,
     gender: 'female',
-    url: 'https://stream.watsonplatform.net/text-to-speech/api/v1/voices/en-US_AllisonVoice',
+    url: 'https://gateway-d.watsonplatform.net/text-to-speech/api/v1/voices/en-US_AllisonVoice',
     supported_features: {
       voice_transformation: false,
       custom_pronunciation: true,
@@ -77,12 +95,28 @@ const voices = [
     },
     description: 'Allison: American English female voice.',
   }, {
+    name: 'en-US_AllisonV2Voice',
+    option: 'American English (en-US): Allison (female, expressive, dnn technology)',
+    language: 'en-US',
+    customizable: true,
+    gender: 'female',
+    url: 'https://gateway-d.watsonplatform.net/text-to-speech/api/v1/voices/en-US_AllisonV2Voice',
+    supported_features: {
+      voice_transformation: false,
+      custom_pronunciation: true
+    },
+    demo: {
+      text: US_TEXT,
+      ssml: US_SSML_DNN
+    },
+    description: "Allison: American English female voice. Dnn technology."
+ }, {
     name: 'fr-FR_ReneeVoice',
     option: 'French (fr-FR): Renee (female)',
     language: 'fr-FR',
     customizable: true,
     gender: 'female',
-    url: 'https://stream.watsonplatform.net/text-to-speech/api/v1/voices/fr-FR_ReneeVoice',
+    url: 'https://gateway-d.watsonplatform.net/text-to-speech/api/v1/voices/fr-FR_ReneeVoice',
     supported_features: {
       voice_transformation: false,
       custom_pronunciation: true,
@@ -98,7 +132,7 @@ const voices = [
     option: 'Italian (it-IT): Francesca (female)',
     customizable: true,
     gender: 'female',
-    url: 'https://stream.watsonplatform.net/text-to-speech/api/v1/voices/it-IT_FrancescaVoice',
+    url: 'https://gateway-d.watsonplatform.net/text-to-speech/api/v1/voices/it-IT_FrancescaVoice',
     supported_features: {
       voice_transformation: false,
       custom_pronunciation: true,
@@ -109,12 +143,28 @@ const voices = [
     },
     description: 'Francesca: Italian (italiano) female voice.',
   }, {
+    name: 'it-IT_FrancescaV2Voice',
+    option: 'Italian (it-IT): Francesca (female, dnn technology)',
+    language: 'it-IT',
+    customizable: true,
+    gender: 'female',
+    url: 'https://gateway-d.watsonplatform.net/text-to-speech/api/v1/voices/it-IT_FrancescaV2Voice',
+    supported_features: {
+       voice_transformation: false,
+       custom_pronunciation: true
+    },
+    demo: {
+      text: IT_TEXT,
+      ssml: IT_SSML_DNN,
+    },
+    description: 'Francesca: Italian (italiano) female voice. Dnn technology.'
+  }, {
     name: 'es-ES_LauraVoice',
     language: 'es-ES',
     option: 'Castilian Spanish (es-ES): Laura (female)',
     customizable: true,
     gender: 'female',
-    url: 'https://stream.watsonplatform.net/text-to-speech/api/v1/voices/es-ES_LauraVoice',
+    url: 'https://gateway-d.watsonplatform.net/text-to-speech/api/v1/voices/es-ES_LauraVoice',
     supported_features: {
       voice_transformation: false,
       custom_pronunciation: true,
@@ -130,7 +180,7 @@ const voices = [
     option: 'German (de-DE): Birgit (female)',
     customizable: true,
     gender: 'female',
-    url: 'https://stream.watsonplatform.net/text-to-speech/api/v1/voices/de-DE_BirgitVoice',
+    url: 'https://gateway-d.watsonplatform.net/text-to-speech/api/v1/voices/de-DE_BirgitVoice',
     supported_features: {
       voice_transformation: false,
       custom_pronunciation: true,
@@ -146,7 +196,7 @@ const voices = [
     language: 'es-ES',
     customizable: true,
     gender: 'male',
-    url: 'https://stream.watsonplatform.net/text-to-speech/api/v1/voices/es-ES_EnriqueVoice',
+    url: 'https://gateway-d.watsonplatform.net/text-to-speech/api/v1/voices/es-ES_EnriqueVoice',
     supported_features: {
       voice_transformation: false,
       custom_pronunciation: true,
@@ -162,7 +212,7 @@ const voices = [
     option: 'German (de-DE): Dieter (male)',
     customizable: true,
     gender: 'male',
-    url: 'https://stream.watsonplatform.net/text-to-speech/api/v1/voices/de-DE_DieterVoice',
+    url: 'https://gateway-d.watsonplatform.net/text-to-speech/api/v1/voices/de-DE_DieterVoice',
     supported_features: {
       voice_transformation: false,
       custom_pronunciation: true,
@@ -178,7 +228,7 @@ const voices = [
     language: 'en-US',
     customizable: true,
     gender: 'female',
-    url: 'https://stream.watsonplatform.net/text-to-speech/api/v1/voices/en-US_LisaVoice',
+    url: 'https://gateway-d.watsonplatform.net/text-to-speech/api/v1/voices/en-US_LisaVoice',
     supported_features: {
       voice_transformation: true,
       custom_pronunciation: true,
@@ -189,13 +239,29 @@ const voices = [
       ssml_voice: US_VOICE_SSML_LISA,
     },
     description: 'Lisa: American English female voice.',
+  },  {
+    name: 'en-US_LisaV2Voice',
+    option: 'American English (en-US): Lisa (female, transformable, dnn technology)',
+    language: 'en-US',
+    customizable: true,
+    gender: 'female',
+    url: 'https://gateway-d.watsonplatform.net/text-to-speech/api/v1/voices/en-US_LisaV2Voice',
+    supported_features: {
+      voice_transformation: false,
+      custom_pronunciation: true,
+    },
+    demo: {
+      text: US_TEXT,
+      ssml: US_SSML_DNN
+    },
+    description: 'Lisa: American English female voice. Dnn technology',
   }, {
     name: 'en-GB_KateVoice',
     option: 'British English (en-GB): Kate (female)',
     language: 'en-GB',
     customizable: true,
     gender: 'female',
-    url: 'https://stream.watsonplatform.net/text-to-speech/api/v1/voices/en-GB_KateVoice',
+    url: 'https://gateway-d.watsonplatform.net/text-to-speech/api/v1/voices/en-GB_KateVoice',
     supported_features: {
       voice_transformation: false,
       custom_pronunciation: true,
@@ -211,7 +277,7 @@ const voices = [
     option: 'North American Spanish (es-US): Sofia (female)',
     customizable: true,
     gender: 'female',
-    url: 'https://stream.watsonplatform.net/text-to-speech/api/v1/voices/es-US_SofiaVoice',
+    url: 'https://gateway-d.watsonplatform.net/text-to-speech/api/v1/voices/es-US_SofiaVoice',
     supported_features: {
       voice_transformation: false,
       custom_pronunciation: true,
@@ -227,7 +293,7 @@ const voices = [
     option: 'Latin American Spanish (es-LA): Sofia (female)',
     customizable: true,
     gender: 'female',
-    url: 'https://stream.watsonplatform.net/text-to-speech/api/v1/voices/es-LA_SofiaVoice',
+    url: 'https://gateway-d.watsonplatform.net/text-to-speech/api/v1/voices/es-LA_SofiaVoice',
     supported_features: {
       voice_transformation: false,
       custom_pronunciation: true,
