1bcce6d47639d09ccb8c59e073a198431a03434b
LEONID RACHEVSKY <rachevsk@us.ibm.com>
2019-03-26 16:04:01 -0400

    changed to make Alison V2 the first (default) voice

--- views/demo.jsx
+++ views/demo.jsx
@@ -48,11 +48,11 @@ export default class Demo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      voice: voices[3], // Alisson is the first voice
+      voice: voices[1], // AlissonV2 is the first voice
       error: null, // the error from calling /classify
-      text: voices[3].demo.text, // default text
-      ssml: voices[3].demo.ssml, // SSML text
-      ssml_voice: voices[3].demo.ssml_voice, // Voice SSML text, only some voices support this
+      text: voices[1].demo.text, // default text
+      ssml: voices[1].demo.ssml, // SSML text
+      ssml_voice: voices[1].demo.ssml_voice, // Voice SSML text, only some voices support this
       ssmlLabel: 'SSML',
       current_tab: 0,
       loading: false,
